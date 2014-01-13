bf7a1000d6eb7069f296a07eb152f0f975dccb59
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2014-01-13 19:26:41 +0100

    Adds a median blur filter

--- nanolinewidth.cpp
+++ nanolinewidth.cpp
@@ -1,6 +1,13 @@
-
 #include <iostream>
 #include <stdexcept>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+void medianBlurElab(cv::Mat_<unsigned char>& img, int k){
+	cv::Mat_<unsigned char> temp;
+	cv::medianBlur(img,temp,k);
+	img = temp.clone();
+}
 
 /************* Main *****************/
 int main(int argc, char *argv[]){
@@ -10,6 +17,16 @@ int main(int argc, char *argv[]){
 			return 1;
 		}
 		
+		cv::Mat_<unsigned char> src = cv::imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+		cv::Mat_<unsigned char> elabImage = src.clone();
+
+		bool medianBlur = true;
+		int medianBlurKernelSize = 3; 
+
+		if(medianBlur)
+			medianBlurElab(elabImage,medianBlurKernelSize);
+
+		cv::imwrite(argv[2], elabImage);
 	}catch(...){
 		return 2;
 	}
