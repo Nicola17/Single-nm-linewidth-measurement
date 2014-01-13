fdec462df312bc88211c229d4357fd085b17714e
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2014-01-13 18:52:43 +0100

    initial main

--- nanolinewidth.cpp
+++ nanolinewidth.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include <stdexcept>
+
+/************* Main *****************/
+int main(int argc, char *argv[]){
+	try{
+		if(argc<3){
+			std::cout << "Wrong number of parameter";
+			return 1;
+		}
+		
+	}catch(...){
+		return 2;
+	}
+	return 0;
+}
